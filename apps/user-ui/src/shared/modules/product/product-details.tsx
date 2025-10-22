15ea2b511d9ff97c9d52ace178c93e537f2eaecd
bhanuprakash026 <bhanutankasala26@gmail.com>
2025-10-22 23:43:34 +0530

    React Image viewer

--- apps/user-ui/src/shared/modules/product/product-details.tsx
+++ apps/user-ui/src/shared/modules/product/product-details.tsx
@@ -1,13 +1,16 @@
-import React from 'react'
+"use client"
+import React, { useState } from 'react'
 
 const ProductDetails = ({ productDetails }: { productDetails: any }) => {
+  const [currentImage, setCurrentImage] = useState(productDetails?.images[0]?.url)
   return (
     <div className='w-full bg-[#f5f5f5] py-5'>
       <div className="w-[90%] bg-white lg:w-[80%] mx-auto pt-6 grid grid-cols-1 lg:grid-cols-[28%_44%_28%] gap-6 overflow-hidden">
-        { /* left column - product images */ }
+        { /* left column - product images */}
         <div className="p-4">
           <div className="relative w-full">
-            Bhanu
+            { /*,Main Image with zoom */}
+            
           </div>
         </div>
       </div>
